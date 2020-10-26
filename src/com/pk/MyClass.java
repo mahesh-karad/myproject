5a8bab0c7afde082ad990b3f390754798e12cfe1
Aayush Kumar <Aayush Kumar@LAPTOP-MO2NCS3P>
2020-10-26 15:16:37 +0530

    This is second commit by Aayush Kumar

--- src/com/pk/MyClass.java
+++ src/com/pk/MyClass.java
@@ -7,7 +7,7 @@ public class MyClass {
     	System.out.println("\n Hi this is Karthik Raman");
     	System.out.println("\n Hi this is Karthik Raman with new changes");
     	System.out.println("Hi This is Vaibhav Garg");
-    	System.out.println("Hi this is Aayush Kumar");
+    	System.out.println("Hello this is Aayush Kumar");
 	}
 
 }
