da6af9db1a35fe1aa223441324ae9568abbb7af9
Karthik Raman <karthik.raman@apisero.com>
2020-10-26 15:27:11 +0530

    This is my third commit

--- src/com/pk/MyClass.java
+++ src/com/pk/MyClass.java
@@ -4,9 +4,7 @@ public class MyClass {
 
 	public static void main(String[] args) {
 	System.out.println("\n Hi this is mahesh karad");
-    	System.out.println("\n Hi this is Karthik Raman");
-    	System.out.println("\n Hi this is Karthik Raman with new changes");
-    	System.out.println("\n Hey this is Namit Wadhwa");
+    	System.out.println("\n Hi this is Raman Karthik");
 	}
 
 }
